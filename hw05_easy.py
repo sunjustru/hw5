91432b53214cd03a2fb42128c1b1f2122d763831
aburov <web@sunjust.ru>
2019-08-31 11:37:13 +0300

    Задача-2 — done

--- hw05_easy.py
+++ hw05_easy.py
@@ -13,17 +13,15 @@ def do_folder(action, folder):
         os.rmdir(folder)
 
 
-# do_folder('make', 'dir_1')
-# do_folder('del', 'dir_1')
-
 files = ['dir_1', 'dir_2', 'dir_3', 'dir_4', 'dir_5', 'dir_6', 'dir_7', 'dir_8', 'dir_9']
-# Создание директорий
-[do_folder('make', itm) for itm in files]
-# Удаление директорий
-[do_folder('del', itm) for itm in files]
+# # Создание директорий
+#[do_folder('make', itm) for itm in files]
+# # Удаление директорий
+# [do_folder('del', itm) for itm in files]
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+os.listdir(path=".")
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
