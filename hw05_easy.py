1304715a1be14ff4b8fa87f9b01af9ab431dc287
aburov <web@sunjust.ru>
2019-08-31 12:29:11 +0300

    раскомментировал для запуска нужные методы

--- hw05_easy.py
+++ hw05_easy.py
@@ -15,9 +15,9 @@ def do_folder(action, folder):
 
 files = ['dir_1', 'dir_2', 'dir_3', 'dir_4', 'dir_5', 'dir_6', 'dir_7', 'dir_8', 'dir_9']
 # # Создание директорий
-#[do_folder('make', itm) for itm in files]
+[do_folder('make', itm) for itm in files]
 # # Удаление директорий
-# [do_folder('del', itm) for itm in files]
+[do_folder('del', itm) for itm in files]
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
