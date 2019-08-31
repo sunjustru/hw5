c93580d9a1b2ae17c8a74ea8c034e3c5b1d516f7
aburov <web@sunjust.ru>
2019-08-31 11:23:44 +0300

    Задача-1: (готово)

--- hw05_easy.py
+++ hw05_easy.py
@@ -1,4 +1,6 @@
 import os
+
+
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
@@ -10,8 +12,15 @@ def do_folder(action, folder):
     else:
         os.rmdir(folder)
 
-do_folder('make', 'dir_1')
-do_folder('del', 'dir_1')
+
+# do_folder('make', 'dir_1')
+# do_folder('del', 'dir_1')
+
+files = ['dir_1', 'dir_2', 'dir_3', 'dir_4', 'dir_5', 'dir_6', 'dir_7', 'dir_8', 'dir_9']
+# Создание директорий
+[do_folder('make', itm) for itm in files]
+# Удаление директорий
+[do_folder('del', itm) for itm in files]
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
