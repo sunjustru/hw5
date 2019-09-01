6bb84e88308961506e19fe21ee672e8a27b94517
aburov <web@sunjust.ru>
2019-09-01 18:33:56 +0300

    промежуточный - готовы все методы, но необходимо поправить LS

--- hw05_hard.py
+++ hw05_hard.py
@@ -16,6 +16,8 @@
 # python with_args.py param1 param2 param3
 import os
 import sys
+import shutil
+
 print('sys.argv = ', sys.argv)
 
 
@@ -23,7 +25,10 @@ def print_help():
     print("help - получение справки")
     print("mkdir <dir_name> - создание директории")
     print("ping - тестовый ключ")
-
+    print("cp <dir_name> - название файла; <file_name> - новое название файла для копирования")
+    print("rm <dir_name> - удаляем указанный файл")
+    print("cd <dir_name> - путь новой директории")
+    print("ls — выводим полный ключ к текущей директории")
 
 def make_dir():
     if not dir_name:
@@ -40,11 +45,58 @@ def make_dir():
 def ping():
     print("pong")
 
+def copy_file():
+    if not dir_name:
+        print("Необходимо указать имя файла который вы хотите скопировать")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        shutil.copy2(dir_name, file_name)
+        print('файл {} скопирован'.format(dir_name))
+    except FileNotFoundError:
+        #TODO: Нет проверки на есть файл с таким названием или нет
+        print('файд {} с таким именем уже существует'.format(dir_name))
+
+def rm_file():
+    if not dir_name:
+        print("Необходимо указать имя файла который вы хотите удалить")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.remove(dir_name)
+        print('файл {} удалён'.format(dir_name))
+    except FileNotFoundError:
+        # TODO: Нет проверки на есть файл с таким названием или нет
+        print('файл {} не найден'.format(dir_name))
+
+def ls_dir():
+    print('Полный путь к текущей директории: ' +os.path.join(os.getcwd()))
+
+def cd_dir():
+    if not dir_name:
+        print("Необходимо директорию для перехода")
+        return
+    try:
+        os.chdir(dir_name)
+        print('вы перешли в директорию {}'.format(dir_name))
+    except FileNotFoundError:
+        # TODO: Нет проверки на есть файл с таким названием или нет
+        print('директория {} не найдена'.format(dir_name))
+
 do = {
     "help": print_help,
     "mkdir": make_dir,
-    "ping": ping
+    "ping": ping,
+    'cp' : copy_file,
+    'rm': rm_file,
+    'cd': cd_dir,
+    'ls': ls_dir
 }
+
+try:
+    file_name = sys.argv[3]
+except IndexError:
+    file_name = None
 
 try:
     dir_name = sys.argv[2]
